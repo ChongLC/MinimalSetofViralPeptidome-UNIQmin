c814cf592a8f6e090a1dbfdd70cd1842fabf5855
ChongLC <51225708+ChongLC@users.noreply.github.com>
2021-01-28 02:02:59 +0800

    Add files via upload

---
+++ U5.2_MinSet.py
@@ -0,0 +1,18 @@
+from Bio import SeqIO
+
+fasta_file = "inputfile.fas" # Input fasta file
+wanted_file = "fileZ.txt" # Input interesting sequence IDs, one per line
+result_file = "FileZ.fasta" # Output fasta file
+
+wanted = set()
+with open (wanted_file) as f: 
+  for line in f: 
+    line = line.strip()
+    if line != "":
+      wanted.add(line)
+
+fasta_sequences = SeqIO.parse(open(fasta_file),'fasta')
+with open (result_file, "w") as f: 
+  for seq in fasta_sequences: 
+    if seq.id in wanted: 
+      SeqIO.write([seq], f, "fasta")
